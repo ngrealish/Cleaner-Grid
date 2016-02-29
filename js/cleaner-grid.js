20e44106d870bdb3472329fa473bd8969f63b76a
Nathan Grealish <nathan.j.grealish@gmail.com>
2016-02-29 09:35:52 -0500

    Updating Example

--- js/cleaner-grid.js
+++ js/cleaner-grid.js
@@ -1,36 +1,36 @@
 $.fn.CleanerGrid = function() {
 	var remainingPercent = 0;
-    return this.each(function() {
-        var numRows = $(this).children().length;
+  return this.each(function() {
+    var numRows = $(this).children().length;
 		var numWidthRows = 0;
 		var totalPercent = 0;
 		var totalWidth = $(this).width();
 		var classes, percent, pixels;
 		$(this).children().each(function(){ 
-			classes = $(this).attr('class').split(" ");
-			for(var i = 0; i < classes.length; i++){
-				if(/^pc-/.test(classes[i])){
-					percent = parseFloat(("."+classes[i].replace("pc-", ""))*100).toFixed(5);
-					numWidthRows++;
-					$(this).css({"width":percent+"%"});
-				} else if(/^px-/.test(classes[i])){
-					pixels = classes[i].replace("px-", "");
-					percent = (pixels/totalWidth)*100;
-					numWidthRows++;
-					$(this).css({"width":pixels+"px"});
-				} else {
-					percent = 0;	
+			if($(this).attr('class')){
+				classes = $(this).attr('class').split(" ");
+				for(var i = 0; i < classes.length; i++){
+					if(/^pc-/.test(classes[i])){
+						percent = parseFloat(("."+classes[i].replace("pc-", ""))*100).toFixed(5);
+						numWidthRows++;
+						$(this).css({"width":percent+"%"});
+					} else if(/^px-/.test(classes[i])){
+						pixels = classes[i].replace("px-", "");
+						percent = (pixels/totalWidth)*100;
+						numWidthRows++;
+						$(this).css({"width":pixels+"px"});
+					} else {
+						percent = 0;	
+					}
+					totalPercent = (parseFloat(totalPercent) + parseFloat(percent));
 				}
-				totalPercent = (parseFloat(totalPercent) + parseFloat(percent));
 			}
 		});
-		console.log(totalPercent);
 		remainingPercent = (100-totalPercent)/(numRows-numWidthRows);
 		$(this).children().each(function(){
 			var styles = $(this).attr('style');
 			var hasWidth = false;
 			if(!styles){
-				console.log($(this), "has a style");	
 				$(this).css({"width":remainingPercent+"%"});	
 			} else {
 				styles = styles.split(";");
@@ -48,5 +48,5 @@ $.fn.CleanerGrid = function() {
 				}
 			}
 		});
-    });
+  });
 };
