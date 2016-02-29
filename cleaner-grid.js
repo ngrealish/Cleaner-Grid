faf9cb67bb46870219a466eaf02b7e4c281ebfce
Nathan Grealish <nathan.j.grealish@gmail.com>
2016-02-28 23:54:08 -0500

    Making Global

--- cleaner-grid.js
+++ cleaner-grid.js
@@ -5,16 +5,15 @@ $.fn.CleanerGrid = function() {
 		var numWidthRows = 0;
 		var totalPercent = 0;
 		var classes, percent, pixels, totalWidth, child;
-		$(this).children().each(function(){ 	
-			child = $(this);			//Each Column
-			classes = $(this).attr('class').split(" ");		//If the column has 'pc' make that column the right width
+		$(this).children().each(function(){
+			classes = $(this).attr('class').split(" ");
 			for(var i = 0; i < classes.length; i++){
 				if(/^pc-/.test(classes[i])){
 					percent = parseFloat(("."+classes[i].replace("pc-", ""))*100).toFixed(5);
 					numWidthRows++;
 				} else if(/^px-/.test(classes[i])){
 					pixels = classes[i].replace("px-", "");
-					totalWidth = $(this).closest('.grid-row').width();
+					totalWidth = $(this).parent().width();
 					percent = parseFloat((pixels/totalWidth)*100).toFixed(5);
 					numWidthRows++;
 				} else {
